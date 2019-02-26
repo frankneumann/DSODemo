c11534fe94d79e145a9e04fcafb1e40ba4fc2a39
neumannf <frank.neumann@htw-berlin.de>
2019-02-26 21:27:21 +0100

    Added menus item to save point cloud

--- app/src/main/java/com/tc/tar/DSOActivity.java
+++ app/src/main/java/com/tc/tar/DSOActivity.java
@@ -1,14 +1,21 @@
 package com.tc.tar;
 
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -20,8 +27,12 @@ import org.rajawali3d.view.ISurface;
 import org.rajawali3d.view.SurfaceView;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
 
 public class DSOActivity extends AppCompatActivity implements DSORenderer.RenderListener {
 
@@ -42,10 +53,17 @@ public class DSOActivity extends AppCompatActivity implements DSORenderer.Render
     private TextView mTextView;
     private long mLastUpdateTime;
     private int mLastUpdateIndex;
+    private MenuItem mItemSavePointCloud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         init();
 
         mRajawaliSurface = createSurfaceView();
@@ -74,8 +92,23 @@ public class DSOActivity extends AppCompatActivity implements DSORenderer.Render
         if (!LOCAL_MODE) {
             mVideoSource.start();
         }
-
         setContentView(mLayout);
+
+        Toolbar toolbar = new Toolbar(this);
+        final TypedArray styledAttributes = this.getTheme().obtainStyledAttributes(
+                new int[] { android.R.attr.actionBarSize });
+        int actionBarSize = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, actionBarSize);
+        toolbar.setLayoutParams(layoutParams);
+        toolbar.setElevation(4f);
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+        toolbar.setVisibility(View.VISIBLE);
+
+        mLayout.addView(toolbar, 0);
+
+        setSupportActionBar(toolbar);
     }
 
     private void init() {
@@ -112,6 +145,54 @@ public class DSOActivity extends AppCompatActivity implements DSORenderer.Render
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        mItemSavePointCloud = menu.add("Save Point Cloud");
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item == mItemSavePointCloud) {
+            String toastMessage = new String();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+            String currentDateandTime = sdf.format(new Date());
+            String filePath = mFileDir + "/PointCloud_" + currentDateandTime + ".ply";
+
+            try {
+                DSOPointCloud pointCloud = TARNativeInterface.dsoGetPointCloud();
+                int pointNum = pointCloud.pointCount;
+                float[] vertices = pointCloud.worldPoints;
+                int[] colors = pointCloud.colors;
+
+                PrintWriter pw = new PrintWriter(filePath);
+                pw.println(String.format("ply\n" +
+                        "format ascii 1.0\n" +
+                        "element vertex %d\n" +
+                        "property float x\n" +
+                        "property float y\n" +
+                        "property float z\n" +
+                        "property uchar red\n" +
+                        "property uchar green\n" +
+                        "property uchar blue\n" +
+                        "end_header", pointNum));
+                for (int i = 0; i < pointNum; i++) {
+                    pw.println(String.format(Locale.ROOT, "%f %f %f %d %d %d", vertices[3 * i], vertices[3 * i + 1], vertices[3 * i + 2],
+                            Color.red(colors[i]), Color.green(colors[i]), Color.blue(colors[i])));
+                }
+                pw.flush();
+                pw.close();
+
+                toastMessage = String.format("Saved point cloud to file: %s", filePath);
+            } catch (Exception e) {
+                toastMessage = String.format("No point cloud could be saved due to exception: %s", e);
+            }
+
+            Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+        }
+
+        return true;
+    }
+
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             mStopped = true;
@@ -128,6 +209,7 @@ public class DSOActivity extends AppCompatActivity implements DSORenderer.Render
             Toast.makeText(this, "Reset!", Toast.LENGTH_SHORT).show();
             return true;
         }
+
         return true;
     }
 
